2155774c8c1dee0aa2f68cdb55d98b9d45bacc76
tobi994 <viettt.fuky@gmail.com>
2015-05-12 21:03:47 +0700

    Enemy

---
+++ Fuky_Jump_Jump/Assets/Scripts/TOBI/Bom.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bom : MonoBehaviour {
+
+    public float speedX;
+    public float speedY;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, speedY);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(-5.0f, 5.0f);
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("ad");
+        if(col.gameObject.tag == "ground")
+        {
+           Debug.Log("ad");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, speedY);
+        }
+    }
+}
---
+++ Fuky_Jump_Jump/Assets/Scripts/TOBI/EnemyController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    private Animator _enemyAnimator;
+
+    private Rigidbody2D _rb2dEnemy;
+
+    public GameObject objectBom;
+
+    public float speed;
+
+    void Start()
+    {
+        _enemyAnimator = GetComponent<Animator>();
+        _rb2dEnemy = GetComponent<Rigidbody2D>();
+        _rb2dEnemy.velocity = new Vector2(-speed, 0f);
+
+    }
+
+    void Update()
+    {
+
+        if (_rb2dEnemy.position.x <= 1.6f)
+        {
+            Attack();
+        }
+        if (_rb2dEnemy.position.x >= 4f)
+        {
+            FlipRight();
+        }
+        //Debug.Log(_rb2dEnemy.velocity);
+    }
+
+    public void Attack()
+    {
+        _enemyAnimator.SetBool("isAttack", true);
+        _rb2dEnemy.velocity = Vector2.zero;
+    }
+
+    public void FlipLeft()
+    {
+        _rb2dEnemy.velocity = new Vector2(speed, 0f);
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+        _enemyAnimator.SetBool("isAttack", false);
+    }
+
+    public void FlipRight()
+    {
+        _rb2dEnemy.velocity = new Vector2(-speed, 0f);
+        Vector3 theScale = transform.localScale;
+        theScale.x = -theScale.x;
+        transform.localScale = theScale;
+    }
+
+    public void CreateBom()
+    {
+       // Instantiate(objectBom, new Vector3(transform.position.x - 1,transform.position.y - 0.18 ,transform.position.z),transform.rotation);
+        Instantiate(objectBom, new Vector3(transform.position.x -1 ,transform.position.y ,transform.position.z), transform.rotation);
+    }
+
+    
+
+}
