a8147804333d2aadcb88265e2912ff347b3be130
toneylee <ducsangpro.93@gmail.com>
2015-05-12 19:15:02 +0700

    Tao truong score

---
+++ Fuky_Jump_Jump/Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoSingleton<GameController> {
+
+    //
+    public GameObject player;
+    public GameObject playerPrefab;
+    public Transform playerPosition;
+    //
+    
+
+    public void GameReStart()
+    {
+
+    }
+
+    //Khoi tao game
+    public void GameInit()
+    {
+        //Tao player
+        if (player == null)
+        {
+            player = Instantiate(playerPrefab);
+            player.transform.position = playerPosition.position;
+        }
+        else
+        {
+            //Reset lai gia tri
+        }
+
+        //Tao enemy
+
+    }
+
+    public void GameUpdate()
+    {
+        //Kiem tra neu chet
+        if (true)
+        {
+            //Load man hinh thua len
+        }
+    }
+
+    //Cong diem
+    public void AddScore(int score)
+    {
+        //Cong diem
+        ScoreController.Instance.ShowScore(score);
+    }
+}
---
+++ Fuky_Jump_Jump/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreController : MonoSingleton<ScoreController> {
+
+    public Text textScore;
+
+    public void ShowScore(int score)
+    {
+        textScore.text = "Score: " + score;
+    }
+}
