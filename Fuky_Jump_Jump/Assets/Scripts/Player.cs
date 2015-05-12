03f419b23f4a80406d09b5e60b5c6a543d39600d
khangnhfuky <khangnhfuky@users.noreply.github.com>
2015-05-12 16:34:39 +0700

    Thêm Player

---
+++ Fuky_Jump_Jump/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private Animator _playerAnimator;
+    private Rigidbody2D _rb2d;
+
+    private bool _grounded;
+
+    public Transform groundCheck;
+    public LayerMask layerGround;
+
+    void Start()
+    {
+        _playerAnimator = GetComponent<Animator>();
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _grounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, layerGround);
+    }
+
+    public void Jump()
+    {
+        _playerAnimator.SetTrigger("jumpPlayer");
+        _rb2d.velocity = new UnityEngine.Vector2(0f, 1f);
+    }
+
+}
